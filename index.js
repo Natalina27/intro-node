071f34b7a90233088c6ddba5f489aee43e269f1b
Natalya Myunster <natalya.myunster@klarna.com>
2024-02-23 16:42:34 +0100

    add Process and Environment

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 console.log('Hello world!')
 //alert('Hello world!') //ReferenceError: alert is not defined
-console.log(window) //browser
-console.log(global) //node
+// console.log(window) //browser -> ReferenceError: window is not defined
+
+//GLOBAL
+// console.log('global', global) //node
+
+//PROCESS
+// console.log('process', process) //node process
+// console.log('process.argv', process.argv)
+
+//PIN
+// console.log('process.pin', process.pin)
+
+//ENV
+// console.log('process.env', process.env)
+console.log(process.env.NODE_ENV)
+
+//EXIT
+// console.log('process.argv', process.exit())
