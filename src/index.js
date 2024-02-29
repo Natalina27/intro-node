7c3652a44f10ceb220b75315dc4d6d424e2c98c3
Natalya Myunster <natalya.myunster@klarna.com>
2024-02-29 21:15:47 +0100

    add newNote

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 #!/usr/bin/env node
 
 console.log('Hello world!')
+
+// const note = process.argv[2]
+// const newNote = {
+//     content: note,
+//     id: Date.now()
+// }
+//
+// console.log('newNote', newNote)
+
 //alert('Hello world!') //ReferenceError: alert is not defined
 // console.log(window) //browser -> ReferenceError: window is not defined
 
@@ -16,7 +25,7 @@ console.log('Hello world!')
 
 //ENV
 // console.log('process.env', process.env)
-//console.log(process.env.NODE_ENV)
+// console.log(process.env.NODE_ENV)
 
 //EXIT
 // console.log('process.argv', process.exit())
