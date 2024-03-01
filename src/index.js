420f70dd079780d4508ebccc48c8f0224b3afaaa
Natalya Myunster <natalya.myunster@klarna.com>
2024-03-01 17:07:45 +0100

    add Modules Overview

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 #!/usr/bin/env node
+import {count} from './utils.js'
 
 console.log('Hello world!')
+console.log(count(5))
+// const note = process.argv[2]
+// const newNote = {
+//     content: note,
+//     id: Date.now(),
+//     id1: new Date().valueOf(),
+//     id2: new Date().getTime()
+// }
+// console.log('process.argv', process.argv)
+// console.log('note', note)
+//console.log('newNote', newNote)
 
-const note = process.argv[2]
-const newNote = {
-    content: note,
-    id: Date.now(),
-    id1: new Date().valueOf(),
-    id2: new Date().getTime()
-}
-console.log('process.argv', process.argv)
-console.log('note', note)
-console.log('newNote', newNote)
+// (function() {
+//     console.log('IIFE')
+// })()
 
 //alert('Hello world!') //ReferenceError: alert is not defined
 // console.log(window) //browser -> ReferenceError: window is not defined
